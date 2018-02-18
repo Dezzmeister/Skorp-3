3d0d97ed68f77fa115abaf8e70dfa6bf5887a3a2
Dezz Meister <dezzy3359@gmail.com>
2018-02-18 02:19:26 -0500

    did som mor stof and maek javadoc! lole

--- src/com/dezzy/skorp3/net/StringActor.java
+++ src/com/dezzy/skorp3/net/StringActor.java
@@ -1,5 +1,13 @@
 package com.dezzy.skorp3.net;
 
+/**
+ * Acts on a String and returns an object of type T. Used in TCPManager.
+ * 
+ * @see TCPManager
+ * @author Dezzmeister
+ *
+ * @param <T> return type
+ */
 @FunctionalInterface
 public interface StringActor<T> {
 	public T act(String s);
