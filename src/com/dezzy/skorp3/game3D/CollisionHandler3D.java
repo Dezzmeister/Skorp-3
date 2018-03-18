49f14a3447d4e3d384983137d65c62cb10883c6d
Dezz Meister <dezzy3359@gmail.com>
2018-03-18 00:04:19 -0400

    yee

--- src/com/dezzy/skorp3/game3D/CollisionHandler3D.java
+++ src/com/dezzy/skorp3/game3D/CollisionHandler3D.java
@@ -6,11 +6,31 @@ import java.util.Map;
 
 import com.dezzy.skorp3.field3D.Entity3D;
 
+@SuppressWarnings("unused")
 public class CollisionHandler3D {
 	private static Map<String,Method> collisionMethods = new HashMap<String,Method>();
+	
+	static {
+		try {
+			for (Method m : CollisionHandler3D.class.getDeclaredMethods()) {
+				collisionMethods.put(m.getName(), m);
+			}
+		} catch (Exception e) {
+			System.out.println("Error occurred loading collision methods.");
+		}
+	}
+	
+	private boolean compositeHitComposite(Entity3D entity1, Entity3D entity2) {
+		return true;
+	}
 	
 	public boolean hasCollided(Entity3D entity1, Entity3D entity2) {
 		
 		return true;
+	}
+	
+	private String format(Entity3D first, Entity3D second) {
+		return first.getShape().name().toLowerCase() + "Hit" + 
+			       second.getShape().name().charAt(0) + second.getShape().name().substring(1).toLowerCase();
 	}
 }
