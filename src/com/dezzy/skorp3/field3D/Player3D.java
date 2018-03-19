696ecb636846ed9053fbfd0435cfeccd8c2d0186
Dezz Meister <dezzy3359@gmail.com>
2018-03-19 00:46:46 -0400

    yolereeeeee

---
+++ src/com/dezzy/skorp3/field3D/Player3D.java
@@ -0,0 +1,9 @@
+package com.dezzy.skorp3.field3D;
+
+public class Player3D extends Composite {
+
+	public Player3D(double x, double y, double z) {
+		super(x, y, z);
+	}
+
+}
--- src/com/dezzy/skorp3/game3D/CollisionHandler3D.java
+++ src/com/dezzy/skorp3/game3D/CollisionHandler3D.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.dezzy.skorp3.field3D.Entity3D;
+import com.dezzy.skorp3.field3D.Geometric3D;
 
 @SuppressWarnings("unused")
 public class CollisionHandler3D {
@@ -20,6 +21,21 @@ public class CollisionHandler3D {
 		}
 	}
 	
+	private boolean rectprismHitRectprism(Geometric3D entity1, Geometric3D entity2) {
+		double halfOfCombinedWidths = (entity1.width + entity2.width) / 2;
+		double halfOfCombinedHeights = (entity1.height + entity2.height) / 2;
+		double halfOfCombinedLengths = (entity1.length + entity2.length) / 2;
+		
+		double xDistance = Math.abs(entity1.point.x - entity2.point.x);
+		double yDistance = Math.abs(entity1.point.y - entity2.point.y);
+		double zDistance = Math.abs(entity1.point.z - entity2.point.z);
+		
+		return (xDistance < halfOfCombinedWidths) ||
+			   (yDistance < halfOfCombinedHeights) ||
+			   (zDistance < halfOfCombinedLengths);
+	}
+	
+	//TODO sort these out
 	private boolean compositeHitComposite(Entity3D entity1, Entity3D entity2) {
 		return true;
 	}
